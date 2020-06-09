a5332d4197ccc18b54ad631e852202b404f6ec14
RohitRajP <RohitRajP>
2020-06-10 01:12:55 +0530

    Completed parsind the response recieved from the UPI payment process

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -6,6 +6,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
   // gets list of UPI applications installed and loads UPI options
   void loadUPIOptions() async {
     var homePageModel = Provider.of<HomePageModel>(context, listen: false);
@@ -23,6 +24,26 @@ class _HomePageState extends State<HomePage> {
         transactionNote: "Payment for ORD1215236",
         transactionRef: "ORD1215236",
         amount: "2.00");
+
+    String message = "";
+    Color sbrColor;
+
+    if (txnResponse.status == UpiTransactionStatus.success) {
+      message = "Transaction Completed!";
+      sbrColor = Colors.green;
+    } else if (txnResponse.status == UpiTransactionStatus.failure) {
+      message = "Transaction Failed!";
+      sbrColor = Colors.red;
+    } else if (txnResponse.status == UpiTransactionStatus.submitted) {
+      message = "Transaction Submitted!";
+      sbrColor = Colors.orange;
+    }
+
+    scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text(message),
+      backgroundColor: sbrColor,
+      duration: Duration(seconds: 3),
+    ));
   }
 
   @override
@@ -35,8 +56,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
-        title: Text("UPI Demonstration"),
+        title: Text("Instant UPI Payment"),
       ),
       body: SafeArea(
         child: homePageBody(),
@@ -66,13 +88,43 @@ class _HomePageState extends State<HomePage> {
   // holds the view after the UPI apps installed have been loaded
   Widget upiAppsLoadedView() {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          demoMessage(),
+          SizedBox(
+            height: 40.0,
+          ),
           upiAppsGridView(),
         ],
+      ),
+    );
+  }
+
+  // holds the message for the demo
+  Widget demoMessage() {
+    return Container(
+      child: RichText(
+        textAlign: TextAlign.left,
+        text: TextSpan(children: [
+          TextSpan(
+            text: "You will be sending ₹2 to UPI ID rrjp@ylb\n\n",
+            style: TextStyle(
+                color: Colors.green,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold),
+          ),
+          TextSpan(
+            text:
+                "Choose any from all the UPI enabled applications installed in your device",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 14.0,
+            ),
+          )
+        ]),
       ),
     );
   }
@@ -81,25 +133,29 @@ class _HomePageState extends State<HomePage> {
   Widget upiAppsGridView() {
     return Consumer<HomePageModel>(
       builder: (context, data, child) {
-        return Container(
-          child: GridView.builder(
-            shrinkWrap: true,
-            itemCount: data.getUPIAppList().length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40.0),
+          child: Container(
+            child: GridView.builder(
+              shrinkWrap: true,
+              itemCount: data.getUPIAppList().length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
-                mainAxisSpacing: 5.0,
-                crossAxisSpacing: 50.0),
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                child: InkWell(
-                  child: Image.memory(data.getUPIAppList()[index].icon,
-                      width: 20, height: 20),
-                  onTap: () {
-                    processTapOnUPIIcon(data.getUPIAppList()[index]);
-                  },
-                ),
-              );
-            },
+                mainAxisSpacing: 20.0,
+                crossAxisSpacing: 30.0,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: InkWell(
+                    child: Image.memory(data.getUPIAppList()[index].icon,
+                        width: 20, height: 20),
+                    onTap: () {
+                      processTapOnUPIIcon(data.getUPIAppList()[index]);
+                    },
+                  ),
+                );
+              },
+            ),
           ),
         );
       },
